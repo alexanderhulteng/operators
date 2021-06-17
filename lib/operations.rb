7e660b4335beeb5869a57a15234980e2dffe1b91
alexanderhulteng <alexanderhulteng@gmail.com>
2021-06-17 14:12:52 -0600

    commit tree e26096d81a412efdb273e6a5fe52821eae6f0cf2

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
+    if speed > 60 || speed < 40
+        true
+    elsif speed < 60 && speed > 40
+        false
+    end
 
 end
 
 
 
 def not_safe?(speed)
-	
+	safe_speed = (speed < 60 && speed > 40? false : true)
 end
 	
 
